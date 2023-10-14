6ca26e4cf41f63606e6b981c3d3f5286004b9953
sharabidinov <alymbeksharabidinov1@gmail.com>
2023-10-15 04:13:36 +0600

    solution

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if (salary < 0) {
+            System.out.println("wrong input!");
+        } else if (salary <= 10_000) {
+            System.out.println(salary * 0.85);
+        } else if (salary <= 18_000) {
+            System.out.println(salary * 0.82);
+        } else if (salary > 20_000) {
+            System.out.println(salary * 0.80);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
-
+        if (month > 0 && month < 4) {
+            System.out.println("Winter");
+        } else if (month > 3 && month < 7) {
+            System.out.println("Spring");
+        } else if (month > 6 && month < 10) {
+            System.out.println("Summer");
+        } else if (month > 9 && month < 13) {
+            System.out.println("Autumn");
+        } else {
+            System.out.println("wrong number!");
+        }
     }
 }
