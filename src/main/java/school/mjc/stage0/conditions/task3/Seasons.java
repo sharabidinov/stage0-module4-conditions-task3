6ffcc941bb4ea249dbcd7e1fb758928fabd19d6b
sharabidinov <alymbeksharabidinov1@gmail.com>
2023-10-15 04:19:01 +0600

    fix

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -4,11 +4,11 @@ public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
         if (month > 0 && month < 4) {
             System.out.println("Winter");
-        } else if (month > 3 && month < 7) {
+        } else if (month > 3 && month < 6) {
             System.out.println("Spring");
-        } else if (month > 6 && month < 10) {
+        } else if (month > 5 && month < 9) {
             System.out.println("Summer");
-        } else if (month > 9 && month < 13) {
+        } else if (month > 8 && month < 13) {
             System.out.println("Autumn");
         } else {
             System.out.println("wrong number!");
